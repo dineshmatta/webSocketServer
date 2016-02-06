4fbb79ca71ca17b598aeb55376a3ce1fb84d5e73
dineshmatta <dineshdm7@gmail.com>
2016-02-07 02:16:52 +0530

    Update server.js
    1 -  Displaying last 10 messages from a channel when any client is connected
    2 - Expiring channel key after 24 hrs from last messages received on channel

--- server.js
+++ server.js
@@ -14,20 +14,19 @@ socketServer( 'example', function ( connection, server ) {
 
   connection.on('open', function ( id ) {
     console.log('[open]');
-    console.log(id);
   });
 
   connection.on('message', function ( msg ) {
     console.log('[message]');
     console.log(msg);
 
-    // Store Data in redis
     var msgData = JSON.parse(msg.utf8Data);
     var channel = msgData.action.data[0].channel;
 
     if(msgData.action.command === 'Top10Msg'){
 
-      client.lrange(channel, 0, -1, function(err, reply) {
+      //Fetch Top 10 messages from a channel, based on 'Top10Msg' command
+      client.lrange(channel, -10, -1, function(err, reply) {
         if(reply && reply.length > 0 ){
 
           reply.forEach(function(data){
@@ -41,12 +40,12 @@ socketServer( 'example', function ( connection, server ) {
 
     } else {
 
-      console.log(Object.keys(server.connections)); // client ids
-      console.log(connection.id); // client id
+      console.log(Object.keys(server.connections)); // All connected client ids
+      console.log(connection.id); // current connected browser client id
 
+      // Store Data in redis agianst channel as uniq key
       client.rpush([channel, msgData], function(err, result){
         if(!err){
-          //connection.sendUTF( msg.utf8Data ); 
           var keys = Object.keys(server.connections);
           for (var j=0; j<keys.length; j++) {
 
@@ -56,8 +55,11 @@ socketServer( 'example', function ( connection, server ) {
             msgData.id = key;
             value.send( JSON.stringify(msgData) );
           } 
-
         }
+
+        // If there are no more messages in the channel, then after 24hrs from the last send message in that channel
+        // All messages in that channel along with complete channel will be delted
+        client.expireat(key, parseInt((+new Date)/1000) + 86400);
       });
 
     }
@@ -69,7 +71,7 @@ socketServer( 'example', function ( connection, server ) {
   });
 
   connection.on('close', function(){
-    // console.log('[close]');
+    //console.log('[close]');
   });
 
 
